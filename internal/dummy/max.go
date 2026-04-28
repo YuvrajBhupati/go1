394228c6b6f58b0c48664c3297087ee12630d960
YuvrajBhupati <66075699+YuvrajBhupati@users.noreply.github.com>
2026-04-28 06:48:25 +0000

    Update max.go

--- internal/dummy/max.go
+++ internal/dummy/max.go
@@ -1,1 +1,5 @@
+package dummy
 
+func A(n int) int { r := 0; for i := 0; i < n; i++ { r += i }; return r }
+func B(n int) int { r := 1; for i := 1; i < n; i++ { r *= i }; return r }
+func C(n int) int { r := 0; for i := 0; i < n; i++ { if i%2==0 { r++ } else { r-- } }; return r }
